3ee45f776205dba640de4df495afe8a56fc7eeef
Nevo Segal <nevosegal@gmail.com>
2015-07-15 17:24:25 +0100

    added a new route - single tool

--- api.js
+++ api.js
@@ -11,6 +11,14 @@ exports.list = function(req,res){
 	});
 }
 
+exports.single = function(req,res){
+	Tool.findOne({'_id': req.params.id}, function(err,tools){
+		if(err) res.send(err.message);
+		res.set('Access-Control-Allow-Origin','*');
+		res.json(tools);
+	});
+}
+
 exports.category = function(req,res){
 	console.log(req.params.name);
 	Tool.find({'category': new RegExp('^'+req.params.name+'$', "i")}, function(err, tools){
@@ -25,7 +33,7 @@ exports.post = function(req,res){
 	var newTool = new Tool(
 				{
 					title: req.body.title, 
-					description:req.body.description
+					description: req.body.description
 				}
 			);
 	newTool.save();
--- routes/api/tools.js
+++ routes/api/tools.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', api.list);
+router.get('/:id', api.single);
 router.get('/category/:name', api.category);
 router.post('/', api.post);
 
